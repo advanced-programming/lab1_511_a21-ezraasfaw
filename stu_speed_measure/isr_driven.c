a062398258589c7cbb3eb8a2c1ec3f8dc202dc36
Ezra-Fikru Asfaw <65423750+ezraasfaw@users.noreply.github.com>
2021-08-31 11:43:12 -0400

    Update isr_driven.c

--- stu_speed_measure/isr_driven.c
+++ stu_speed_measure/isr_driven.c
@@ -14,14 +14,28 @@
  *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 #include <xc.h>
 #include <sys/attribs.h>
+
+/*Global variables*/
+signed int of = 0;
+extern int timePeriod= 0;
+                    
 void __ISR( _TIMER_2_VECTOR, IPL1SOFT) T2InterruptHandler( void){ 
      IFS0bits.T2IF = 0; 
-    
+     of++; // Timer overflows
 } 
 
-void __ISR( _CHANGE_NOTICE_VECTOR, IPL2SOFT) CNInterrupt( void){
-     IFS1bits.CNIF = 0;//Clear the CN flag?
+//void __ISR( _CHANGE_NOTICE_VECTOR, IPL2SOFT) CNInterrupt( void){
+//     IFS1bits.CNIF = 0;//Clear the CN flag?
 
 // Do something?
-
+/*Interrupt Service Routine for Change Notification */
+     void __ISR( _CHANGE_NOTICE_VECTOR, IPL2SOFT) CNInterrupt(void){
+         static signed  start,final;
+         if(PORTGbits.RG7){// Rising edge detection
+            final=TMR2;
+            timePeriod= (of*0x10000)+final-start;
+            start=final; 
+            of=0; 
+         }
+         IFS1bits.CNIF=0; //clear the change notification interrupt bit
 }
