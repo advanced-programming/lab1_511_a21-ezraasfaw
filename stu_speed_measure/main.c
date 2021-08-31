77972aa872c76d7db223c0813d2ac6b54564a5b3
Ezra-Fikru Asfaw <65423750+ezraasfaw@users.noreply.github.com>
2021-08-31 11:43:45 -0400

    Update main.c

--- stu_speed_measure/main.c
+++ stu_speed_measure/main.c
@@ -7,9 +7,9 @@
       
  * * REVISION HISTORY:
  *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Author        	Date      	Comments on this revision
+ * Author                 Date      	Comments on this revision
  *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~            
- *  Ezra-Fikru Asfaw      24 Aug. 2021		                              
+ * Ezra-Fikru Asfaw      24 Aug. 2021		                              
  *
  *      
  *
@@ -21,15 +21,24 @@
 #include "console32.h"
 #include "Tick_core.h"
 
-
+//int frequency;
+//int buf[999];
+//extern int timePeriod;
 
 int main( void){
     // Initialisation starts here
 	initT2();
     initCN9 ();
     //Super loop starts here
-    while( 1){				 	
-	}//while(1))  //  
+    initUart1();
+    //fprintf2(C_UART1,"Hello\n");
+    
+    while(1){
+//        delay_us(1000);
+//        frequency = 100000/timePeriod; // 1/(timePeriod* 10 uS)}
+//        sprintf(buf, "frequency: %d\n", frequency);
+//        fprintf2(C_UART1, buf);
+    }
+    
 
 }// main
-
